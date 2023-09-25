930bac93184622ddb5d0da80366a2e2eabb8b43a
Jom Karlo Verzosa <jomkarlov@gmail.com>
2023-09-25 23:04:09 +0800

    feat: used button props and button groups

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,4 +1,5 @@
-import { Button, Stack } from "@mui/material"
+import { Button, Stack, IconButton, ButtonGroup } from "@mui/material"
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
 
 export const MuiButton = () => {
   return (
@@ -15,6 +16,29 @@ export const MuiButton = () => {
         <Button variant="outlined" color="warning">warning</Button>
         <Button variant="outlined" color="info">info</Button>
         <Button variant="outlined" color="success">success</Button>
+      </Stack>
+      <Stack display="block" spacing={4} direction="row">
+        <Button variant="contained" size="small">Small</Button>
+        <Button variant="contained" size="medium">Medium</Button>
+        <Button variant="contained" size="large">Large</Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" startIcon={<ShoppingCartIcon />} disableRipple onClick={() => alert("cliccccck")}>
+          Cart
+        </Button>
+        <Button variant="contained" endIcon={<ShoppingCartIcon />} disableElevation>
+          Cart
+        </Button>
+        <IconButton aria-label="send" color="secondary" size="large">
+          <ShoppingCartIcon />
+        </IconButton>
+      </Stack>
+      <Stack direction="row">
+        <ButtonGroup variant="contained" color="secondary" size="small" orientation="vertical" aria-label="alignment button group">
+          <Button>Left</Button>
+          <Button disableRipple>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
       </Stack>
     </Stack>
     
