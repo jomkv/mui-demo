ccf1a47bc9c1034566582110b1e0fb029430e9d8
Jom Karlo Verzosa <jomkarlov@gmail.com>
2023-09-25 20:55:39 +0800

    init commit

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material"
+
+export const MuiTypography = () => {
+  return (
+    <>
+      <Typography variant="h1">h1 Heading</Typography>
+      <Typography variant="h2">h2 Heading</Typography>
+      <Typography variant="h3">h3 Heading</Typography>
+    </>
+  )
+}
